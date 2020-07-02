0172e0ddc2f9fb78bd592ce1a98ac23187f604eb
Bovenko Leonid <leonidb@ikantam.com>
2020-07-02 22:35:45 +0300

    feat: fix bugs in statistics view. IPPSARE-177

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
 
     'name' => env('APP_NAME', 'Velmie reports'),
 
-    'version' => '20200612',// used as stamp for asset files
+    'version' => '20200720',// used as stamp for asset files
 
     /*
     |--------------------------------------------------------------------------
